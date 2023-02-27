c717005685acd5e2548203ad8696773accd566be
sy <sykoivisto@gmail.com>
2023-02-26 17:14:24 -0700

    add scoreboard

--- src/Components/Gameboard/Gameboard.js
+++ src/Components/Gameboard/Gameboard.js
@@ -24,21 +24,37 @@ const Gameboard = () => {
   // game should be over when a player wins or there are no turns left i.e. a tie
 
   return (
-    <div className={styles.gameboard}>
-      <div className={styles.row}>
-        <Gridsquare value={gameState[0][0]} square={[0,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[0][1]} square={[0,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[0][2]} square={[0,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+    <div>
+      <div className={styles.scoreboard}>
+        <div className={`${styles.player1} ${playerTurn === 1 ? styles.active : null}`}>
+          Player 1
+          <div className={styles.score}>
+            {player1Score}
+          </div>
+        </div>
+        <div className={`${styles.player2} ${playerTurn === 2 ? styles.active : null}`}>
+          Player 2
+          <div className={styles.score}>
+            {player2Score}
+          </div>
+        </div>
       </div>
-      <div className={styles.row}>
-        <Gridsquare value={gameState[1][0]} square={[1,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[1][1]} square={[1,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[1][2]} square={[1,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-      </div>
-      <div className={styles.row}>
-        <Gridsquare value={gameState[2][0]} square={[2,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[2][1]} square={[2,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
-        <Gridsquare value={gameState[2][2]} square={[2,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+      <div className={styles.gameboard}>
+        <div className={styles.row}>
+          <Gridsquare value={gameState[0][0]} square={[0,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[0][1]} square={[0,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[0][2]} square={[0,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+        </div>
+        <div className={styles.row}>
+          <Gridsquare value={gameState[1][0]} square={[1,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[1][1]} square={[1,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[1][2]} square={[1,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+        </div>
+        <div className={styles.row}>
+          <Gridsquare value={gameState[2][0]} square={[2,0]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[2][1]} square={[2,1]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+          <Gridsquare value={gameState[2][2]} square={[2,2]} onHandlePlayerClick={onHandlePlayerClick}></Gridsquare>
+        </div>
       </div>
     </div>
   );
